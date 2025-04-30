ff4609ce260bd339000b25c9f1d86c4c00989443
sung-ik-je <ij16_21@naver.com>
2025-05-01 08:06:08 +0900

    chagne scheduler time because mac sleep mode

--- backend/config/scheduler.py
+++ backend/config/scheduler.py
@@ -8,8 +8,8 @@ from backend.services.stock_service import make_stock_charts
 scheduler = AsyncIOScheduler()
 
 def start_scheduler():
-    trigger = CronTrigger(hour=6, minute=30, timezone='Asia/Seoul')
-    scheduler.add_job(make_stock_charts, trigger)
+    trigger = CronTrigger(hour=7, minute=00, timezone='Asia/Seoul')
+    scheduler.add_job(make_stock_charts, trigger, misfire_grace_time=3000)
     scheduler.start()
 
 def stop_scheduler():
