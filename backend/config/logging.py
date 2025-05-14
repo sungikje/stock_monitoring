a21fb1fda9dd4e72b56702f9912e17f3e23def82
sung-ik-je <ij16_21@naver.com>
2025-05-14 19:19:05 +0900

    add requirements.txt, fix log file path

--- backend/config/logging.py
+++ backend/config/logging.py
@@ -9,11 +9,14 @@ LOG_FORMAT = "[%(asctime)s] [%(levelname)s] [%(name)s] - %(pathname)s, %(lineno)
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 log_file_name = datetime.today().strftime("%Y-%m-%d") + '.log'
 
+today_log_path = os.path.join(BASE_DIR, LOG_PATH)
+os.makedirs(today_log_path, exist_ok=True)
+
 logging.basicConfig(
     level=logging.INFO,
     format=LOG_FORMAT,
     datefmt=DATE_FORMAT,
-    filename=os.path.join(BASE_DIR, LOG_PATH, log_file_name),
+    filename=os.path.join(today_log_path, log_file_name),
     filemode="a",
 )
 
